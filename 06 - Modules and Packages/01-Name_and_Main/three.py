df928d06e2f29d5ddc33895b6c9578f5166c55fe
Darren Glynn <darren.glynn@gmail.com>
2019-11-09 12:30:18 +0200

    Add Name and Main examples

---
+++ 06 - Modules and Packages/01-Name_and_Main/three.py
@@ -0,0 +1,16 @@
+#three.py
+
+def func():
+    print("FUNC() IN ONE.PY")
+
+def function():
+    pass
+
+def function2():
+    pass
+
+if __name__ == '__main__':
+    # Run the script!
+    function2()
+    function()
+    func()
